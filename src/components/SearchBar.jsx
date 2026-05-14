bb5a8b8047d09bb1a522343aaf7f4228c79a074d
MaxGAUBERT <120033370+MaxGAUBERT@users.noreply.github.com>
2026-05-14 09:10:36 +0100

    Add files via upload

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,8 +5,7 @@
 export default function SearchBar({ searchTerm, setSearchTerm }) {
 
     return (
-        <div className='absolute right-50 top-5 w-1/4'>
-            <h2 className='bg-gray-800 fixed top-0 left-0 w-full'> World shop - Search </h2>
+        <div className='fixed top-0 left-0'>
             <div className='flex space-x-4 mt-16 p-4'>
                 <input
                     type='text'
