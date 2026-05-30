e97c51ca3fd669d6daeaf9c6a9a4526593041362
MaxGAUBERT <120033370+MaxGAUBERT@users.noreply.github.com>
2026-05-30 11:31:53 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import axios from "axios";
 import Home from "./components/Home";
 import Admin from "./components/Admin";
+import useProducts from "./hooks/useProducts";
+import useCategories from "./hooks/useCategories";
 
 function App() {
-  const [products, setProducts] = useState([]);
+  const products = useProducts();
+  const categories = useCategories();
   const [cart, setCart] = useState([]);
 
   function handleAddItem(product) {
@@ -16,17 +19,23 @@ function App() {
     setCart((prev) => prev.filter((item) => item.id !== productId));
   }
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/api/products")
-      .then((res) => setProducts(res.data))
-      .catch((err) => console.error(err));
-  }, []);
-
+  console.log("categories:", categories);
+  
   return (
   <Routes>
-    <Route path="/" element={<Home products={products} cart={cart} setCart={setCart} />} />
+    <Route path="/" element={<Home products={products} cart={cart} setCart={setCart} categories={categories}/>} />
     <Route path="/admin" element={<Admin />} />
+     <Route
+        path="/api/categories"
+        element={
+          <Home
+            products={products}
+            cart={cart}
+            setCart={setCart}
+            categories={categories}
+          />
+        }
+      />
   </Routes>
   );
 }
