73fa3718325b9e75127c5a7936307acc95d35e7b
MaxGAUBERT <120033370+MaxGAUBERT@users.noreply.github.com>
2026-06-28 17:23:30 +0200

    Add files via upload

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,13 +1,13 @@
 export default function Category({ categories = [], onSelect }) {
   return (
-    <div className="bg-green-500 flex gap-2">
+    <div className="flex flex-shrink-0 bg-white-800">
       <button onClick={() => onSelect("all")} className="cursor-pointer">
         All
       </button>
 
       {categories.map((cat) => (
         <div
-          className="cursor-pointer p-2 bg-gray-600"
+          className="cursor-pointer p-2 hover:bg-gray-600"
           onClick={() => onSelect(cat)}
         >
           {cat.name}
